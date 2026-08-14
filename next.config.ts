976ea2ee33046494d8fec7e3128b4b8a549e55d3
KyleManuel <129800638+KyleManuel@users.noreply.github.com>
2026-08-14 16:07:28 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const isProd = process.env.NODE_ENV === "production";
 
+const nextConfig: NextConfig = {
   output: "export",
 
+  basePath: isProd ? "/portfolio" : "",
+
+  assetPrefix: isProd ? "/portfolio/" : "",
 };
 
 export default nextConfig;
