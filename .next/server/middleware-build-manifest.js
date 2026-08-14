0afc026dca9414af70405dbebf24dec217a76229
KyleManuel <129800638+KyleManuel@users.noreply.github.com>
2026-08-14 15:54:07 -0700

    build

---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/0cz1d0mv5g_q7.js"
+  ],
+  "lowPriorityFiles": [
+    "static/XcLHXsY4DkvzdcZkQqpeA/_buildManifest.js",
+    "static/XcLHXsY4DkvzdcZkQqpeA/_ssgManifest.js",
+    "static/XcLHXsY4DkvzdcZkQqpeA/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/2nwqoerny79-z.js",
+    "static/chunks/33g7_zjejejur.js",
+    "static/chunks/1upe53-127sm4.js",
+    "static/chunks/turbopack-1q3phkz2kg7dc.js"
+  ]
+};
